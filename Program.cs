1d8b93592e60d381c36758427cfecc6597ea7df2
Viacheslav <v_iakovlev@list.ru>
2022-08-05 19:01:09 +0300

    Method GSA added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// Написать программу, которая из имеющенгося массива строк формирует массив строк, длина которых меньше или равна 3 символа.
 
+
+// Метод создания пользовательского массива строк из 5 элементов.
+string[] GetStringArray(string a, string b,string c,string d,string e) 
+{
+    string[] result = new string[5] {a,b,c,d,e};
+    return result;
+}
+
